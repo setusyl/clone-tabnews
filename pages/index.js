f167426b8c1d702670e3a6802049894d6c9a9431
setusyl <ribeirosy2@gmail.com>
2024-10-26 20:55:59 +0000

    alteração no arquivo pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -2,4 +2,4 @@ function Home () {
     return <h1>Oi meninas lindas e maravilhosas
         , me dê um OK se receber essa mensagem! Beijinhos</h1>
 }
-export default Home; git 
+export default Home; 
