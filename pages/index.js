19f8ebd1677e78f0aabe8e82d66a7944adffed2e
setusyl <ribeirosy2@gmail.com>
2024-11-02 22:42:16 -0300

    testando o vscode no computador

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home () {
-    return <h1>Oi meninas lindas e maravilhosas, estou fazendo um site e se receberem esta mensagem, me responda com um OK no meu whatsapp! Beijinhos Sylvia</h1>
+    return <h1>Oi meninas lindas e maravilhosas, vocês são nota 100000000! Beijinhos Sylvia</h1>
 }
 export default Home; 
